7e4b271a5b0dc1f2a9a098948766266182ab9678
Gloacky <selimattia0@gmail.com>
2026-03-07 15:58:25 +0100

    Fixed prisma issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "../app/generated/prisma";
-const prisma = new PrismaClient();
+import "dotenv/config";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+import { PrismaClient } from "@prisma/client";
+
 
 async function main() {
     const demoUserId = "133767f0-768d-4338-a612-50c8dc722b84";
